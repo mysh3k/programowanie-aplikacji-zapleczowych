4347b7eb63ca2eef7830064f0b6765ee9f033909
PiwkaNorbert <71353104+PiwkaNorbert@users.noreply.github.com>
2023-05-25 14:58:04 +0200

    Update config.jsx
    do localhosta

--- frontend-app/src/config.jsx
+++ frontend-app/src/config.jsx
@@ -1,4 +1,4 @@
-export const API_URL = `http://192.168.15.115:7777/`;
-export const API_URL_2 = `http://192.168.15.115:8888/`;
+export const API_URL = `http://localhost:7777/`;
+export const API_URL_2 = `http://localhost:8888/`;
 
 export let TIMEOUT_SEC = 3;
